4f6e46b5d4d00a54af6e2206a6316f2cccada131
diqian <qiandi.eric@gmail.com>
2017-06-05 23:37:29 -0700

    add

--- Z_MERN_BEST_PRACTICE/client/src/index.js
+++ Z_MERN_BEST_PRACTICE/client/src/index.js
@@ -5,6 +5,7 @@ import Routes from './routes';
 
 filepicker.setKey('YOUR_API_KEY');
 
+// Our views are rendered inside the #content div
 ReactDOM.render(
   Routes,
   document.getElementById('content')
